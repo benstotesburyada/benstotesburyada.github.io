5b6dce002d23f8f6447de4406069de3e715674b3
benstotesburyada <31472724+benstotesburyada@users.noreply.github.com>
2018-02-27 20:19:21 +0000

    skeletonNormalise

---
+++ Skeleton-2.0.4/draw.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(500,500);
+  background(255,255,255);
+  fill(255,0,0);
+  rect(50,400,40,40);
+  fill(0,0,255);
+  ellipse(200,200,200,200);
+}
+
+function draw() {
+  /* no need for code here at this stage*/
+}
