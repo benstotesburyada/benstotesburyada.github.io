a87984f022a827ad6dfdaaeea76a203211163a9c
benstotesburyada <31472724+benstotesburyada@users.noreply.github.com>
2018-03-13 14:34:23 +0000

    wewerew

--- draw.js
+++ draw.js
@@ -1,35 +1,31 @@
-var x = 180;
-var y = 180;
-var xspeed = 9;
-var yspeed = 10;
 
+var x = 360;
+var y = 360;
+var xspeed = 90;
+var yspeed = 20;
 
 function setup() {
-  createCanvas(500, 300);
+  createCanvas(windowWidth, windowHeight);
   background (25);
-
 }
 
 function draw() {
+  background(random(50),random(50),random(50));
 
-  //distance from center of the screen
-  var d = dist(windowWidth/2, windowHeight/2, mouseX, mouseY);
+  var d = random(0,250);
 
-  fill (random(200, 250), random(200, 250), random(200, 250));
+  fill (random(0, 255), random(0, 255), random(0, 255));
   ellipse (x, y, d, d);
 
-  //bouncing horizontally
   x = x + xspeed;
-
   if (x > windowWidth || x < 0)  {
     xspeed = -xspeed;
   }
-
-  //bouncing veritcally
   y = y + yspeed;
-
   if (y > windowHeight || y < 0) {
     yspeed = -yspeed;
   }
-
 }
+
+var snd = new Audio("file.mp3"); // buffers automatically when created
+snd.play();
